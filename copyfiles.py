4d7919d78a706e9408dbeb8f9deec196d014b173
Jason Dugger <jasondugger@icloud.com>
2024-02-29 07:04:50 -0600

    saving

--- copyfiles.py
+++ copyfiles.py
@@ -1,11 +1,13 @@
 import csv
 import os
-import shutil
+import pathlib
 import sys
-import time
+import shutil
 
-fpath = "Enter the directory do you want to copy the files FROM: "
-dpath = "Enter the directory do you want to copy the files TO: "
+# spath = input("Enter the directory do you want to copy the files FROM: ")
+# dpath = input("Enter the directory do you want to copy the files TO: ")
+spath = "c:/Users/jdugg/Documents"
+dpath = "c:/Users/jdugg/Documents/testfiles"
 
 print("Add the list of packet/file names you want to copy:\n")
 
@@ -15,7 +17,16 @@ while True:
     if not filen:
         break
     flist_in.append(filen)
-print(flist_in)
 
-# if flist_in.count < 1:
-#     sys.exit("No files to copy. Exiting.")
+if len(flist_in) == 0:
+    sys.exit("No files to copy. Exiting.\n")
+
+for item in flist_in:
+    try:
+        shutil.copyfile(f"{spath}/{item}", f"{dpath}/{item}")
+        print(f"copied {spath}/{item} to {dpath}/{item}")
+    except FileNotFoundError as e:
+        print(f"{e}")
+
+
+print()
--- renamefiles.py
+++ renamefiles.py
@@ -1,9 +1,34 @@
 import csv
-import os
-import shutil
-import sys
 import time
+import pathlib
+import sys
+import csv
 
-# Path: copyfiles.py
-# This script copies files from a source directory to a destination directory
+# spath = input("Enter the directory do you want to copy the files FROM: ")
+# dpath = input("Enter the directory do you want to copy the files TO: ")
+spath = "c:/Users/jdugg/Documents"
+dpath = "c:/Users/jdugg/Documents/testfiles"
+csvfilename = "c:/Users/jdugg/Documents/testfiles/packets.csv"
 
+myfiles = pathlib.Path(spath)
+csvfile = open(csvfilename, 'w', newline='')
+spamwriter = csv.writer(csvfile, delimiter=' ',
+                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
+spamwriter.writerow(['old name', 'new name'])
+
+print()
+
+if len(f"{[i.name for i in myfiles.iterdir()]}") < 1:
+    sys.exit("No files to rename. Exiting.\n")
+
+for item in myfiles.iterdir():
+    try:
+        if item.is_file() and item.suffix == '.pdf' and item.name.startswith("R".upper()):
+            print(f"renaming {item.name}")
+            spamwriter.writerow([item.name, item.name])
+            time.sleep(.125)
+
+    except FileNotFoundError as e:
+        print(f"{e}")
+
+print()
