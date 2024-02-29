a04d49d187859cd907223cef3bf554fcdb5e4285
Jason Dugger <jasondugger@icloud.com>
2024-02-29 12:46:09 -0600

    saving

--- renamefiles.py
+++ renamefiles.py
@@ -2,15 +2,21 @@ import csv
 import time
 import pathlib
 import sys
+import os
+import re
+import glob
 import csv
 
-# spath = input("Enter the directory do you want to copy the files FROM: ")
-# dpath = input("Enter the directory do you want to copy the files TO: ")
-spath = "c:/Users/jdugg/Documents"
-dpath = "c:/Users/jdugg/Documents/testfiles"
-csvfilename = "c:/Users/jdugg/Documents/testfiles/packets.csv"
+spath = input("Enter the directory with the files to be renamed: ")
+if not spath:
+    spath = "c:/Users/jdugg/Documents"
+    print(f"defaulting to directory {spath}")
+else:
+    print(f"using directory {spath}")
 
+csvfilename = "c:/Users/jdugg/Documents/testfiles/packets.csv"
 myfiles = pathlib.Path(spath)
+print(f"myfiles {myfiles}")
 csvfile = open(csvfilename, 'w', newline='')
 spamwriter = csv.writer(csvfile, delimiter=' ',
                         quotechar='|', quoting=csv.QUOTE_MINIMAL)
@@ -24,7 +30,15 @@ if len(f"{[i.name for i in myfiles.iterdir()]}") < 1:
 for item in myfiles.iterdir():
     try:
         if item.is_file() and item.suffix == '.pdf' and item.name.startswith("R".upper()):
+            print(f"file date {dir(myfiles)}")
+
             print(f"renaming {item.name}")
+            # os.rename('old_name.txt', 'new_name.txt')
+            regex_pattern = re.compile(r'^IMG_(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})\.jpg$')
+            match = regex_pattern.match(filename)
+            if match:
+                year, month, day, hour, minute, second = match.groups()
+
             spamwriter.writerow([item.name, item.name])
             time.sleep(.125)
 
